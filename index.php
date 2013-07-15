cfdf71c8de91098c60011c18394916310e5586d6
Ben Keen <ben.keen@gmail.com>
2013-07-14 22:29:54 -0700

    sync

--- index.php
+++ index.php
@@ -88,53 +88,7 @@
 		This site is not affiliated with <a href="http://ebird.org" target="_blank">eBird</a>.
 	</footer>
 
-	<div class="hidden">
-		<div id="about">
-			<h2>About eBirdsearch.org</h2>
-			<p>
-				I created eBirdsearch.org to fill what I regard as a rather conspicuous gap in the functionality of the amazing 
-				<a href="http://ebird.org" target="_blank">eBird.org</a> site.
-			</p>
-			<p>
-				If you haven't discovered eBird yet, do so now. It's an incredible resource and tool for birders, ornithologists, educators - 
-				anyone interested in birds. Birders around the world submit millions of observations a year to eBird, providing a wealth of
-				information for viewing and analysis. The more people use it, the better it becomes.
-			</p>
-			<p>
-				The problem is, the search functionality currently available through eBird is fairly limited. As a birder, I wanted a clear 
-				and high-level overview of a region: where are the popular hotspots? What locations yield the most birds? Which are most 
-				common? This site attempts to help plug that gap.
-			</p>
-			<p>
-				All the code for this site is open source and <a href="https://github.com/benkeen/eBirdAdvancedSearch" target="_blank">free for 
-				anyone to download and play with</a>. I've endeavoured to keep the number of data requests to eBird's site to a minimum
-				so it doesn't put too much of a stress on their servers, but if it does, they're entirely within their rights to ask me to 
-				take the site down. Hopefully it won't come to that. I very much hope that one day this sort of functionality will be available 
-				directly through eBird, but in the meantime I'll try to keep the site up and running.
-			</p>
-			<p>
-				Have fun! 
-			</p>
-			<p>
-				- <a href="http://www.benjaminkeen.com" target="_blank">Ben Keen</a>, March 2013
-			</p>
-		</div>
-
-		<div id="contact">
-			<h2>Contact</h2>
-
-			<p>
-				Found a bug? Got an idea on how to improve this site? I'd love to hear from you.
-			</p>
-			<p>
-				Make a comment on <a href="http://www.benjaminkeen.com/ebirdsearch-org/">this post</a>, or email me at 
-				<a href="mailto:ben.keen@gmail.com">ben.keen@gmail.com</a>.
-			</p>
-			<p>
-				Thanks!
-			</p>
-		</div>
-	</div>
+	<?php @include_once("about.php"); ?>
 	<?php @include_once("tracking.php"); ?>
 
 </body>
