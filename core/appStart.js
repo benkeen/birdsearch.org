403c0a8c48c35d52e7f732ea6f7e88187b9f5286
Benjamin Keen <ben.keen@gmail.com>
2013-08-28 09:18:01 -0700

    Wrapped appStart in onload (possible error with loading tablesort prior to other resources)

--- core/appStart.js
+++ core/appStart.js
@@ -9,27 +9,29 @@ require([
 ], function(mediator) {
 	"use strict";
 
-	$.tablesorter.addParser({
-		id: "customdate",
-		is: function() { return false; },
-		format: function(s, table, cell, cellIndex) {
-			var u = $(cell).data("u");
-			if (u) {
-				return parseInt(u, 10);
-			}
-		},
-		type: 'numeric'
-	});
+	$(function() {
+		$.tablesorter.addParser({
+			id: "customdate",
+			is: function() { return false; },
+			format: function(s, table, cell, cellIndex) {
+				var u = $(cell).data("u");
+				if (u) {
+					return parseInt(u, 10);
+				}
+			},
+			type: 'numeric'
+		});
 
-	$.tablesorter.addParser({
-		id: 'species',
-		is: function() { return false; },
-		format: function(s, table, cell) {
-			return $(cell).find("div").text();
-		},
-		type: 'numeric'
-	});
+		$.tablesorter.addParser({
+			id: 'species',
+			is: function() { return false; },
+			format: function(s, table, cell) {
+				return $(cell).find("div").text();
+			},
+			type: 'numeric'
+		});
 
-	// start 'er up!
-	mediator.start();
+		// start 'er up!
+		mediator.start();
+	});
 });
